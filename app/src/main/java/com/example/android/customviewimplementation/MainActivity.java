93b82f390adeb3306bcb3782355a93af033e9fe2
ROHAN PRASAD <34542787+rohap97@users.noreply.github.com>
2019-01-30 01:06:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/customviewimplementation/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.customviewimplementation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
